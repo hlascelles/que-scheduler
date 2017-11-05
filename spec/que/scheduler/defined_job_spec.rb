af21478f14b7a9cef3eadc4b4e2b7f6ed072e65a
Harry Lascelles <harry@harrylascelles.com>
2017-11-05 16:14:43 +0000

    Code quality updates

--- spec/que/scheduler/defined_job_spec.rb
+++ spec/que/scheduler/defined_job_spec.rb
@@ -2,64 +2,64 @@ require 'spec_helper'
 
 RSpec.describe Que::Scheduler::DefinedJob do
   describe '#next_run_time' do
-    let(:job) {
+    let(:job) do
       described_class.create(
         name: 'HalfHourlyTestJob',
         job_class: HalfHourlyTestJob,
         cron: '14 17 * * *'
       )
-    }
+    end
 
     def expect_time(from, to, exp)
       expect(job.next_run_time(Time.zone.parse(from), Time.zone.parse(to))).to eq(exp)
     end
 
-    it "calculates the next run time over a day" do
+    it 'calculates the next run time over a day' do
       expect_time('2017-10-08T06:10:00', '2017-10-09T06:10:00', Time.zone.parse('2017-10-08T17:14:00'))
     end
 
-    it "calculates the next run time under a day" do
+    it 'calculates the next run time under a day' do
       expect_time('2017-10-08T06:10:00', '2017-10-08T21:10:00', Time.zone.parse('2017-10-08T17:14:00'))
     end
 
-    it "calculates the next run starting from exactly cron time" do
+    it 'calculates the next run starting from exactly cron time' do
       expect_time('2017-10-08T17:14:00', '2017-10-09T21:10:00', Time.zone.parse('2017-10-09T17:14:00'))
     end
 
-    it "calculates the next run when it is after the closing time" do
+    it 'calculates the next run when it is after the closing time' do
       expect_time('2017-10-08T06:14:00', '2017-10-08T10:10:00', nil)
     end
   end
 
   describe 'validations' do
     it 'checks the cron is valid' do
-      expect {
+      expect do
         described_class.create(
           name: 'HalfHourlyTestJob',
           job_class: HalfHourlyTestJob,
-          cron: 'foo 17 * * *',
+          cron: 'foo 17 * * *'
         )
-      }.to raise_error(/Invalid cron 'foo 17 \* \* \*' in que-scheduler config/)
+      end.to raise_error(/Invalid cron 'foo 17 \* \* \*' in que-scheduler config/)
     end
 
     it 'checks the queue is a string' do
-      expect {
+      expect do
         described_class.create(
           name: 'HalfHourlyTestJob',
           job_class: HalfHourlyTestJob,
-          queue: 3214214
+          queue: 3_214_214
         )
-      }.to raise_error(/Invalid queue '3214214' in que-scheduler config/)
+      end.to raise_error(/Invalid queue '3214214' in que-scheduler config/)
     end
 
     it 'checks the priority is an integer' do
-      expect {
+      expect do
         described_class.create(
           name: 'HalfHourlyTestJob',
           job_class: HalfHourlyTestJob,
           priority: 'foo'
         )
-      }.to raise_error(/Invalid priority 'foo' in que-scheduler config/)
+      end.to raise_error(/Invalid priority 'foo' in que-scheduler config/)
     end
   end
 end
