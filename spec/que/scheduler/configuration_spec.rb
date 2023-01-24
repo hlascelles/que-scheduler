47eab3dcce767cecc959c56857a944048f88b9aa
Harry Lascelles <harry@harryl.com>
2023-01-24 21:21:34 +0000

    Add more tests
    This adds more tests and uses the `dememoize` gem.

--- spec/que/scheduler/configuration_spec.rb
+++ spec/que/scheduler/configuration_spec.rb
@@ -32,4 +32,39 @@ RSpec.describe Que::Scheduler::Configuration do
       expect(Que::Scheduler.configuration.transaction_adapter).to eq(::Que.method(:transaction))
     end
   end
+
+  describe ".apply_defaults" do
+    it "sets the schedule_location to the default" do
+      Que::Scheduler.apply_defaults
+      expect(Que::Scheduler.configuration.schedule_location).to eq("config/que_schedule.yml")
+    end
+
+    it "sets the que_scheduler_queue to the default" do
+      Que::Scheduler.apply_defaults
+      expect(Que::Scheduler.configuration.que_scheduler_queue)
+        .to eq(Que::Scheduler::VersionSupport.default_scheduler_queue)
+    end
+
+    it "sets the transaction_adapter to the default" do
+      Que::Scheduler.apply_defaults
+      expect(Que::Scheduler.configuration.transaction_adapter).to eq(::Que.method(:transaction))
+    end
+
+    it "sets the schedule to nil" do
+      Que::Scheduler.apply_defaults
+      expect(Que::Scheduler.configuration.schedule).to be_nil
+    end
+
+    it "sets the time_zone to nil" do
+      Que::Scheduler.apply_defaults
+      expect(Que::Scheduler.configuration.time_zone).to be_nil
+    end
+
+    it "sets the schedule_location to the QUE_SCHEDULER_CONFIG_LOCATION ENV" do
+      ClimateControl.modify(QUE_SCHEDULER_CONFIG_LOCATION: "baz.yml") do
+        Que::Scheduler.apply_defaults
+      end
+      expect(Que::Scheduler.configuration.schedule_location).to eq("baz.yml")
+    end
+  end
 end
